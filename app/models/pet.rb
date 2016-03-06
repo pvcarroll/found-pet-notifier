d46defde95eb35f9d1b335524c83ff351142c75b
Paul Carroll <pvcarroll@gmail.com>
2016-03-06 13:23:10 -0600

    try query with typeString'

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,14 @@
 class Pet < ActiveRecord::Base
   def find_pet
+    typeString = "type = #{self.animalType}"
+    puts typeString
     if Rails.env.production?
       client = SODA::Client.new({:domain => "data.austintexas.gov", :app_token => "9lzsGmTO9Jp03lNdi1Db7JvJ6"})
       puts "client ", client.inspect
-      response = client.get("kz4x-q9k5", {"$where" => "type = #{self.animalType}"})
-      puts "response ", response.inspect
+      typeString = "type = #{self.animalType}"
+      puts typeString
+      response = client.get("kz4x-q9k5", {"$where" => "'"typeString"'"})
+      # puts "response ", response.inspect
     else
 
     end
