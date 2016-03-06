b651f9b0cbe67ff287fe7238cbc1e04829cda2f1
Paul Carroll <pvcarroll@gmail.com>
2016-03-06 16:36:50 -0600

    rm puts

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -8,13 +8,12 @@ class Pet < ActiveRecord::Base
       response = client.get("kz4x-q9k5", {"$where" => typeString})
       # puts "response ", response.inspect
       response.each do |hashie|
-        puts "HASHIE: "
-        puts hashie.looks_like
-        puts self.breed
         looks_like = hashie.looks_like.downcase
         color = hashie.color.downcase
         if (looks_like.include? self.breed) && (color.include? self.color)
           puts "looks like #{self.breed} and color like #{self.color}"
+
+          # Mail self.email
         end
       end
     else
