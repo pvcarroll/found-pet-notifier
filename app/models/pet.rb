cda8332fa5c6d4476cc4ffcae7b1dd829a1e3f67
Paul Carroll <pvcarroll@gmail.com>
2016-03-06 15:21:18 -0600

    puts hashie.looks_like

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -9,7 +9,7 @@ class Pet < ActiveRecord::Base
       # puts "response ", response.inspect
       response.each do |hashie|
         puts "HASHIE: "
-        # puts hashie.looks_like
+        puts hashie.looks_like
         # puts self.breed
         # if hashie.looks_like.include? self.breed
         #   puts "looks like #{self.breed}"
