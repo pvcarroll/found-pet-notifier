0e241480119bd80a51e638bbdff5875302d057a4
Paul Carroll <pvcarroll@gmail.com>
2016-03-06 14:34:40 -0600

    fix looks_like query

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,7 +5,7 @@ class Pet < ActiveRecord::Base
     if Rails.env.production?
       client = SODA::Client.new({:domain => "data.austintexas.gov", :app_token => "9lzsGmTO9Jp03lNdi1Db7JvJ6"})
       puts "client ", client.inspect
-      typeString = "type = '#{self.animalType}' AND sex = '#{self.sex}' AND looks_like like '%#{self.breed}'%"
+      typeString = "type = '#{self.animalType}' AND sex = '#{self.sex}' AND looks_like like '%#{self.breed}%'"
       puts typeString
       response = client.get("kz4x-q9k5", {"$where" => typeString})
       puts "response ", response.inspect
