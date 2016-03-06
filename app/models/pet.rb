80d46e731eb1293dc501f620243bc0c116250aa3
Paul Carroll <pvcarroll@gmail.com>
2016-03-06 14:48:18 -0600

    remove looks_like from query, add OR sex = 'unknown'

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,10 +5,13 @@ class Pet < ActiveRecord::Base
     if Rails.env.production?
       client = SODA::Client.new({:domain => "data.austintexas.gov", :app_token => "9lzsGmTO9Jp03lNdi1Db7JvJ6"})
       puts "client ", client.inspect
-      typeString = "type = '#{self.animalType}' AND sex = '#{self.sex}' AND looks_like like '#{self.breed}'"
+      typeString = "type = '#{self.animalType}' AND (sex = '#{self.sex}' OR sex = 'unknown')"
       puts typeString
       response = client.get("kz4x-q9k5", {"$where" => typeString})
-      puts "response ", response.inspect
+      # puts "response ", response.inspect
+      response.each do |hashie|
+        puts "HASHIE: ", hashie
+      end
     else
 
     end
