a31098b0d55824aabccb98018f949ad505e79d7e
Paul Carroll <pvcarroll@gmail.com>
2016-03-06 13:44:22 -0600

    fix query

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,9 +5,9 @@ class Pet < ActiveRecord::Base
     if Rails.env.production?
       client = SODA::Client.new({:domain => "data.austintexas.gov", :app_token => "9lzsGmTO9Jp03lNdi1Db7JvJ6"})
       puts "client ", client.inspect
-      typeString = "type = #{self.animalType}"
+      typeString = "type = '#{self.animalType}'"
       puts typeString
-      response = client.get("kz4x-q9k5", {"$where" => 'typeString'})
+      response = client.get("kz4x-q9k5", {"$where" => typeString})
       # puts "response ", response.inspect
     else
 
