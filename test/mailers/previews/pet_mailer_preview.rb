def25a1adb199e2557e379fd3602d6b56c20caff
paulcarroll <pvcarroll@gmail.com>
2016-03-26 16:05:32 -0500

    send email from dev env

--- test/mailers/previews/pet_mailer_preview.rb
+++ test/mailers/previews/pet_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/pet_mailer
 class PetMailerPreview < ActionMailer::Preview
-
+  def sample_mail_preview
+    PetMailer.send_email(Pet.first)
+  end
 end
