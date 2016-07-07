01876548cb63fb152a2b48826621339145db2ce8
paulcarroll <pvcarroll@gmail.com>
2016-07-07 17:57:33 -0500

    sendgrid instead of gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,13 +16,13 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => ENV['GMAIL_USERNAME'],
-      :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => "plain",
+  ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'localhost:3000',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
       :enable_starttls_auto => true
   }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,13 +64,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => ENV['GMAIL_USERNAME'],
-      :password             => ENV['GMAIL_PASSWORD'],
-      :authentication       => "plain",
+  ActionMailer::Base.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'localhost:3000',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
       :enable_starttls_auto => true
   }
 
